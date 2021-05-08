9971bfe1c6af97818e3d5b121d9b465812c4b3ec
Przemysław Wrzeszcz <przemyslaw.wrzeszcz@gmail.com>
2021-05-08 17:16:33 +0200

    use handle exceptions

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, NotFoundException} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,8 +17,25 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((item) => item.id === +id);
+    const coffee = this.coffees.find((item) => item.id === +id);
+
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+
+    return coffee;
   }
+
+  /*General exception used
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+
+    if (!coffee) {
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+    return coffee;
+  }*/
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
