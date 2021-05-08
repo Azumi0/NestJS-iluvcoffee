03bba428b13571fe95372df75c5242b4919a5617
Przemysław Wrzeszcz <przemyslaw.wrzeszcz@gmail.com>
2021-05-08 17:36:58 +0200

    request validation

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -12,6 +12,8 @@ import {
   // Res,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import {CreateCoffeeDto} from "./dto/create-coffee.dto";
+import {UpdateCoffeeDto} from "./dto/update-coffee.dto";
 
 @Controller('coffees')
 export class CoffeesController {
@@ -41,8 +43,8 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Post('/deprecated')
@@ -52,8 +54,8 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
