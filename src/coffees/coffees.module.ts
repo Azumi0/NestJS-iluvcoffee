c815708f558b24c3f3e00d4fc707335c54431d15
Przemysław Wrzeszcz <przemyslaw.wrzeszcz@gmail.com>
2021-05-09 12:34:53 +0200

    remove mock

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,17 +6,10 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
-class MockCoffeesService {}
-
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [
-    {
-      provide: CoffeesService,
-      useValue: new MockCoffeesService(),
-    },
-  ],
+  providers: [CoffeesService],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
